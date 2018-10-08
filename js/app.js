5155126ed8a647a86281c1276e9ca9836aba80c8
Ryan Jones <ryan.jones@wearenova.co.uk>
2018-10-08 16:50:24 +0100

    Rendering shapes

--- js/app.js
+++ js/app.js
@@ -4,11 +4,28 @@ const app = new WHS.App([
   new WHS.DefineModule(
     "camera",
     new WHS.PerspectiveCamera({
-      position: new THREE.Vector3(0, 0, 0)
+      position: {
+        y: 10,
+        z: 50
+      }
     })
   ),
   new WHS.RenderingModule({ bgColor: 0x162129 }),
   new WHS.ResizeModule()
 ]);
+
+const box = new WHS.Box({
+  geometry: {
+    width: 20,
+    height: 20,
+    depth: 20
+  },
+  material: new THREE.MeshBasicMaterial({
+    color: 0xffffff
+  }),
+  position: [0, 0, 0]
+});
+
+box.addTo(app);
 
 app.start();

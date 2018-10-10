b7d1bbe5bb077eef336875cef715e7bf096bcf22
Ryan Jones <ryan.jones@wearenova.co.uk>
2018-10-10 17:22:00 +0100

    Linter config

--- src/app.js
+++ src/app.js
@@ -7,17 +7,12 @@ import {
   LineSegments,
   LineBasicMaterial,
   MeshBasicMaterial,
-  Mesh
-} from "three";
-import { black, purple, lightSkyBlue } from "./colors";
+  Mesh,
+} from 'three';
+import { black, purple, lightSkyBlue } from './colors';
 
 const scene = new Scene();
-const camera = new PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  100
-);
+const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
 
 const renderer = new WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -30,10 +25,7 @@ const cube = new Mesh(geometry, material);
 scene.add(cube);
 
 const edges = new EdgesGeometry(geometry);
-const line = new LineSegments(
-  edges,
-  new LineBasicMaterial({ color: lightSkyBlue })
-);
+const line = new LineSegments(edges, new LineBasicMaterial({ color: lightSkyBlue }));
 
 cube.add(line);
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  entry: "./src/app.js",
+  entry: './src/app.js',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js"
-  }
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
 };

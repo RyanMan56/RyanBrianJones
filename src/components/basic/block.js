dbb04f8bf0bcb7375443030d5e12f97e9d01cd20
Ryan <ryanman56@gmail.com>
2019-11-10 23:25:17 +0000

    Added first road markings

--- src/components/basic/block.js
+++ src/components/basic/block.js
@@ -6,20 +6,22 @@ import { richBlack } from '../../colors';
 class Block {
   constructor({
     color = richBlack,
+    emissive = 0x000000,
     position = new Vector3(0, 0, 0),
     dimensions = new Vector3(0, 0, 0),
   }) {
     this.color = color;
+    this.emissive = emissive;
     this.position = position;
     this.dimensions = dimensions;
     return this.create();
   }
 
   create() {
-    const { color, position, dimensions } = this;
+    const { color, emissive, position, dimensions } = this;
     const geometry = new BoxGeometry(dimensions.x, dimensions.y, dimensions.z);
+    const material = new MeshPhongMaterial({ color, emissive });
 
-    const material = new MeshPhongMaterial({ color });
     const cube = new Mesh(geometry, material);
     cube.position.set(position.x, position.y, position.z);
 

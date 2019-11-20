9207acd22c4f3b3986260ba4d2fb776daf884e62
Ryan <ryanman56@gmail.com>
2019-11-20 18:10:56 +0000

    MASSIVE IMPROVEMENT: Changed UI tiles to all use the same WebGLRenderer. Scissoring the 3D tiles into their respective locations

--- src/ui/tile.js
+++ src/ui/tile.js
@@ -1,42 +1,36 @@
-import { WebGLRenderer, Scene, OrthographicCamera, Vector3, AmbientLight, Clock } from 'three';
+import { Scene, OrthographicCamera, Vector3, AmbientLight } from 'three';
 import { addDirectionalLight } from '../helpers';
-import { richBlack } from '../colors';
 
 class Tile {
   constructor(tileObject) {
+    this.tileObject = tileObject;
+
     this.scene = null;
-    this.renderer = null;
+    this.camera = null;
     this.element = null;
 
-    return this.init(tileObject);
+    return this.init();
   }
 
-  init(tileObject) {
+  init() {
     this.element = document.createElement('div');
     this.element.className = 'tile-item';
 
-    const canvas = document.createElement('canvas');
+    const canvas = document.createElement('div');
     canvas.className = 'tile-item-canvas';
-    canvas.width = 138;
-    canvas.height = 138;
     this.element.appendChild(canvas);
 
-    this.renderer = new WebGLRenderer({ canvas, antialias: true });
-    this.renderer.setSize(canvas.width, canvas.height);
     this.scene = new Scene();
     const d = 3;
-    const camera = new OrthographicCamera(-d, d, d, -d, -100, 100);
-    camera.position.set(1.5, -0.5, 0);
-    camera.rotation.order = 'YXZ';
-    camera.rotation.y = -Math.PI / 4;
-    camera.rotation.x = Math.atan(-1 / Math.sqrt(2));
-    camera.rotation.z = 0;
+    this.camera = new OrthographicCamera(-d, d, d, -d, -100, 100);
+    this.camera.position.set(1.5, -0.5, 0);
+    this.camera.rotation.order = 'YXZ';
+    this.camera.rotation.y = -Math.PI / 4;
+    this.camera.rotation.x = Math.atan(-1 / Math.sqrt(2));
+    this.camera.rotation.z = 0;
 
     this.scene.add(new AmbientLight(0xffffff, 1));
-    this.renderer.setClearColor(richBlack, 1);
-    this.renderer.setPixelRatio(window.devicePixelRatio);
-
-    this.scene.add(tileObject.group);
+    this.scene.add(this.tileObject.group);
 
     addDirectionalLight({
       scene: this.scene,
@@ -45,24 +39,10 @@ class Tile {
       position: new Vector3(0, 7, 5.1),
     });
 
-    this.renderer.render(this.scene, camera);
-
     const title = document.createElement('span');
     title.className = 'tile-item-title';
-    title.textContent = tileObject.name;
+    title.textContent = this.tileObject.name;
     this.element.appendChild(title);
-
-    const clock = new Clock();
-    clock.start();
-
-    const animate = () => {
-      if (this.renderer) {
-        requestAnimationFrame(animate);
-        tileObject.update(clock);
-        this.renderer.render(this.scene, camera);
-      }
-    };
-    animate();
 
     return this;
   }
--- src/ui/ui-manager.js
+++ src/ui/ui-manager.js
@@ -1,5 +1,7 @@
+import { WebGLRenderer, Clock } from 'three';
 import Tile from './tile';
 import { NO_BUILDINGS } from '../helpers/tile-types';
+import { richBlack } from '../colors';
 
 class UIManager {
   constructor({ tileFactory = null }) {
@@ -8,10 +10,20 @@ class UIManager {
     this.tileTypes = null;
     this.uiTiles = [];
     this.selectedTile = null;
+    this.canvas = null;
+    this.renderer = null;
+
+    this.resizeRendererToDisplaySize.bind(this);
     this.init();
   }
 
   init() {
+    this.canvas = document.getElementById('ui-canvas');
+    const tileItems = document.getElementById('tile-items');
+    this.renderer = new WebGLRenderer({ canvas: this.canvas, antialias: true, alpha: true });
+    this.renderer.setSize(tileItems.clientWidth, tileItems.clientHeight);
+    this.renderer.setClearColor(0x000000, 0);
+
     const currentType = NO_BUILDINGS;
 
     this.tileTypes = {
@@ -32,11 +44,15 @@ class UIManager {
       tileHeadings.appendChild(element);
     });
 
+    const clock = new Clock();
+    clock.start();
+
     this.addUIForTileType(currentType);
+    requestAnimationFrame(() => this.render(clock));
   }
 
   addUIForTileType(currentType) {
-    const tileItems = document.getElementById('tile-items');
+    const tileItems = document.getElementById('tile-items-list');
     this.tileTypes[currentType].items.forEach((tileObject) => {
       const tile = new Tile(tileObject);
       this.uiTiles.push(tile);
@@ -56,16 +72,10 @@ class UIManager {
   }
 
   onHeadingClick(type, element) {
-    const tileItems = document.getElementById('tile-items');
+    const tileItems = document.getElementById('tile-items-list');
 
     while (this.uiTiles.length > 0) {
-      // TODO: This is really really bad. Contexts aren't being properly destroyed, we're just fooling js.
-      // Needed though, otherwise the main scene (more specifically the main WebGLRenderingContext) is automatically disposed.
       this.uiTiles[0].scene.dispose();
-      this.uiTiles[0].renderer.forceContextLoss();
-      this.uiTiles[0].renderer.dispose();
-      this.uiTiles[0].renderer.domElement = null;
-      this.uiTiles[0].renderer = null;
       this.uiTiles.shift();
     }
     while (tileItems.lastChild) {
@@ -79,6 +89,53 @@ class UIManager {
 
     element.className = 'tile-type-selected';
     this.addUIForTileType(type);
+  }
+
+  resizeRendererToDisplaySize(renderer) {
+    const tileItems = document.getElementById('tile-items');
+    const width = tileItems.clientWidth;
+    const height = tileItems.clientHeight;
+    if (this.canvas.width !== width || this.canvas.height !== height) {
+      renderer.setSize(width, height);
+    }
+  }
+
+  renderTile(item, clock) {
+    const { scene, camera, element, tileObject } = item;
+    
+    tileObject.update(clock);
+    const canvasArea = element.querySelector('.tile-item-canvas');
+
+    const { left: tileLeft, bottom: tileBottom, width: tileWidth, height: tileHeight } = canvasArea.getBoundingClientRect();
+    const { left: areaLeft, bottom: areaBottom } = document.getElementById('tile-items').getBoundingClientRect();
+      
+      
+    camera.aspect = tileWidth / tileHeight;
+    camera.updateProjectionMatrix();
+    
+    const positiveYUpTileBottom = window.innerHeight - tileBottom;
+    const positiveYUpAreaBottom = window.innerHeight - areaBottom;
+
+    this.renderer.setScissor(tileLeft - areaLeft, positiveYUpTileBottom - positiveYUpAreaBottom, tileWidth, tileHeight);
+    this.renderer.setViewport(tileLeft - areaLeft, positiveYUpTileBottom - positiveYUpAreaBottom, tileWidth, tileHeight);
+      
+    this.renderer.render(scene, camera);
+  }
+
+  render(clock) {
+    this.resizeRendererToDisplaySize(this.renderer);
+
+    this.renderer.setClearColor(0x000000, 0);
+    this.renderer.setScissorTest(false);
+    this.renderer.clear(true, true);
+    this.renderer.setScissorTest(true);
+    this.renderer.setClearColor(richBlack, 1);
+
+    this.uiTiles.forEach((item) => {
+      this.renderTile(item, clock);
+    });
+
+    requestAnimationFrame(() => this.render(clock));
   }
 }
 
